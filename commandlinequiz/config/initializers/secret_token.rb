63a474d6bd2538143fcaba16d998f860c4588124
Brandon Walsh <bmw9t@virginia.edu>
2014-08-05 11:12:04 -0400

    Deletes rails app.

--- commandlinequiz/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Commandlinequiz::Application.config.secret_token = '2f1ef9d0b33d44959871c2bb29a60d5d5a23611da38d0fb596d50c0870d365bb31a01dc29753e66e324f06fdf1794c991785b086020d253795180ba2004f7034'
